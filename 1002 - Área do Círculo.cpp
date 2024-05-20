e1a8e287f745f619eb35f6fad6cdc0a826f3a352
RaniFerreira <129872650+RaniFerreira@users.noreply.github.com>
2024-05-20 11:50:46 -0300

    Add files via upload

---
+++ 1002 - Área do Círculo.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+A fórmula para calcular a área de uma circunferência é: area = π . raio2. 
+Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+Saída
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, 
+com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double).
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
+caso contrário, você receberá "Presentation Error".
+
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double raio, area;
+    
+    cin >> raio;
+    
+    area = 3.14159 * pow(raio, 2);
+    
+    
+    cout << "A=" << fixed << setprecision(4) << area << endl;
+
+    return 0;
+}
